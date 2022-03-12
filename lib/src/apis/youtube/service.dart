4e7f6e53ee85f674316e0e2974ee408f43ffafca
OlehMarch <oleh.marchenko.95@gmail.com>
2022-03-12 23:22:56 +0200

    reverted revert of awaits in recursions

--- lib/src/apis/youtube/service.dart
+++ lib/src/apis/youtube/service.dart
@@ -198,7 +198,7 @@ class YouTubeService {
         log('Login session ended. Creating new one...');
         await loginSilently();
         log('New login session created. Retrying channel reporting...');
-        reportChannelVideos(
+        await reportChannelVideos(
           id: id,
           username: username,
           customUrl: customUrl,
@@ -224,7 +224,7 @@ class YouTubeService {
       if (e.message == youtubeReportAbuseTooManyRqErrorMessage) {
         log('Retrying in ${_retryDelay.inMinutes} minutes...');
         await Future.delayed(_retryDelay);
-        reportVideo(id);
+        await reportVideo(id);
         return;
       }
       if (e.message == youtubeReportAbuseVideoNotFoundErrorMessage) {
@@ -239,7 +239,7 @@ class YouTubeService {
         log('Login session ended. Creating new one...');
         await loginSilently();
         log('New login session created. Retrying video reporting...');
-        reportVideo(id);
+        await reportVideo(id);
         return;
       }
 
