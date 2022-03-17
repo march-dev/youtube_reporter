4c39d071fae28616270e9df23948f507fe6c9b0e
OlehMarch <oleh.marchenko.95@gmail.com>
2022-03-17 19:54:10 +0200

    removed redundant logging

--- lib/src/utils/datetime.dart
+++ lib/src/utils/datetime.dart
@@ -1,15 +1,8 @@
-import 'logger.dart';
-
 const ptTimezoneOffset = Duration(hours: -7);
 
 Duration ptTimeToNextDay() {
   final nowUtc = DateTime.now().toUtc();
   final ptNow = nowUtc.add(ptTimezoneOffset);
   final ptNextDay = DateTime(ptNow.year, ptNow.month, ptNow.day + 1);
-  final diff = ptNextDay.difference(ptNow);
-  log(nowUtc.toString());
-  log(ptNow.toString());
-  log(ptNextDay.toString());
-  log(diff.toString());
   return ptNextDay.difference(ptNow);
 }
